caedc6d4fc9e47a738d656312dfee1b2dbe8825e
Cezary Wojcieszuk <s22798@pjwstk.edu.pl>
2023-09-05 23:23:18 +0200

    Added AddTemplateCommandValidator.cs

---
+++ Gymify.Application/Templates/Commands/AddTemplate/AddTemplateCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Gymify.Application.Exercises.Queries.ExerciseUidExistence;
+using Gymify.Application.Users.Queries.UserUIdExistence;
+using Gymify.Domain.Constants.Column;
+using MediatR;
+
+namespace Gymify.Application.Templates.Commands.AddTemplate;
+
+public class AddTemplateCommandValidator: AbstractValidator<AddTemplateCommand>
+{
+    public AddTemplateCommandValidator(IMediator mediator)
+    {
+        RuleFor(x => x.UserUid)
+            .MustAsync(async (x, token) => await mediator.Send(new UserUidExistenceQuery(x), token))
+            .WithMessage("User doesnt exist");
+
+        RuleFor(x => x.TemplateName)
+            .MaximumLength(TemplateColumnConstants.TemplateNameLimit);
+
+        RuleForEach(x => x.Exercises.Select(e => e.Comments))
+            .MaximumLength(TemplateColumnConstants.TemplateCommentsLimit);
+
+        RuleForEach(x => x.Exercises.Select(e => e.ExerciseUid))
+            .MustAsync(async (x, token) => await mediator.Send(new ExerciseUidExistenceQuery(x), token));
+    }
+}
