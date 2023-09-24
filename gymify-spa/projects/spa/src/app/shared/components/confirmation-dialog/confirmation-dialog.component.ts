2cbfc493ba47b1ec57a4996fa5dfe8e3da6b1b3d
Cezary Wojcieszuk <s22798@pjwstk.edu.pl>
2023-09-24 20:10:38 +0200

    changed styling in confirmation dialog

--- gymify-spa/projects/spa/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
+++ gymify-spa/projects/spa/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -2,18 +2,19 @@ import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'gym-confirmation-dialog',
   standalone: true,
-  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, MatIconModule],
   templateUrl: './confirmation-dialog.component.html',
   styleUrls: ['./confirmation-dialog.component.scss'],
 })
 export class ConfirmationDialogComponent {
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: { title: string, message: string },
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string },
     private dialogRef: MatDialogRef<ConfirmationDialogComponent>
   ) {}
 
