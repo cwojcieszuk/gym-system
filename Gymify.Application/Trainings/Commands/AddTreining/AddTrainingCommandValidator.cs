45f88392f1a901f7adb4b56fe0ad1fe65e059007
mssroka <msroka1108@gmail.com>
2023-09-27 23:36:09 +0200

    add training command validator

--- Gymify.Application/Trainings/Commands/AddTreining/AddTrainingCommandValidator.cs
+++ Gymify.Application/Trainings/Commands/AddTreining/AddTrainingCommandValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Gymify.Application.Users.Queries.UserUIdExistence;
+using Gymify.Domain.Constants.Column;
 using MediatR;
 
 namespace Gymify.Application.Trainings.Commands.AddTreining;
@@ -8,6 +9,11 @@ public class AddTrainingCommandValidator : AbstractValidator<AddTrainingCommand>
 {
     public AddTrainingCommandValidator(IMediator mediator)
     {
-        
+        RuleFor(x => x.UserUid)
+            .MustAsync(async (x, token) => await mediator.Send(new UserUidExistenceQuery(x), token))
+            .WithMessage("User doesn't exist");
+
+        RuleFor(x => x.TrainingName)
+            .MaximumLength(TrainingColumnConstants.TrainingNameLimit);
     }
 }
