670e76854cf4edf8ed26b6aa95a1540e58be50da
Cezary Wojcieszuk <s22798@pjwstk.edu.pl>
2023-10-02 17:52:48 +0200

    added roles seed migration

--- Gymify.Persistence/Migrations/20231002155104_addedRolesSeeds.Designer.cs
+++ Gymify.Persistence/Migrations/20231002155104_addedRolesSeeds.Designer.cs
@@ -4,6 +4,7 @@ using Gymify.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Gymify.Persistence.Migrations
 {
     [DbContext(typeof(GymifyDbContext))]
-    partial class GymifyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231002155104_addedRolesSeeds")]
+    partial class addedRolesSeeds
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
