d481975b16ff5fc9479b05e5bbde222432b2aa4c
Cezary Wojcieszuk <s22798@pjwstk.edu.pl>
2023-10-07 13:16:32 +0200

    added coaches list endpoint

---
+++ GymifyApi/Controllers/CoachesController.cs
@@ -0,0 +1,34 @@
+﻿using Gymify.Application.Coaches.Queries;
+using GymifyApi.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymifyApi.Controllers;
+
+[ApiController]
+[Route("api/coaches")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class CoachesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CoachesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCoaches([FromQuery] GetCoachesQuery request)
+    {
+        Guid userUid = Guid.Parse(User.GetUserUid());
+
+        if (userUid == null)
+        {
+            return Forbid();
+        }
+
+        return Ok(await _mediator.Send(request with { UserUid = userUid }));
+    }
+}
