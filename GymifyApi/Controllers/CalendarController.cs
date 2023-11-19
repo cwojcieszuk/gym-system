eb68353763bc2f1b4bb78caf2f714291616190d6
Cezary Wojcieszuk <czarekwojcieszuk61@gmail.com>
2023-11-19 15:24:53 +0100

    added add coach hour endpoint

--- GymifyApi/Controllers/CalendarController.cs
+++ GymifyApi/Controllers/CalendarController.cs
@@ -1,4 +1,6 @@
-﻿using Gymify.Application.Calendar.Queries.GetCalendarEvents;
+﻿using Gymify.Application.Calendar.Commands.AddCoachHour;
+using Gymify.Application.Calendar.Queries.GetCalendarEvents;
+using Gymify.Shared.Constants;
 using GymifyApi.Extensions;
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -30,5 +32,22 @@ public class CalendarController : ControllerBase
         }
 
         return Ok(await _mediator.Send(request with { UserUid = userUid }));
+    }
+
+    [HttpPost]    
+    [Route("coach-hours")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = $"{RoleConstants.Admin},{RoleConstants.Coach}")]
+    public async Task<IActionResult> AddCoachHour([FromBody] AddCoachHourCommand request)
+    {
+        Guid userUid = Guid.Parse(User.GetUserUid());
+
+        if (userUid == null)
+        {
+            return Forbid();
+        }
+
+        await _mediator.Send(request with { CoachUid = userUid });
+        
+        return NoContent();
     }
 }
