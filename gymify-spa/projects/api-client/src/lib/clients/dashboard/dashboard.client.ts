2e7932e631699c732a709d576faa473a6de3d3e3
Cezary Wojcieszuk <czarekwojcieszuk61@gmail.com>
2023-11-28 11:33:33 +0100

    added amount to dashboard requests

--- gymify-spa/projects/api-client/src/lib/clients/dashboard/dashboard.client.ts
+++ gymify-spa/projects/api-client/src/lib/clients/dashboard/dashboard.client.ts
@@ -9,19 +9,20 @@ import { RecentTemplateDTO } from './models/recent-template.dto';
 @Injectable({ providedIn: 'root' })
 export class DashboardClient {
   private readonly url = 'dashboard';
+  private readonly amount = 3;
 
   constructor(private http: HttpClient) {}
 
   getPopularCoaches(): Observable<PopularCoachDTO[]> {
-    return this.http.get<PopularCoachDTO[]>(`${this.url}/popular-coaches`);
+    return this.http.get<PopularCoachDTO[]>(`${this.url}/popular-coaches`, { params: { amount: this.amount } });
   }
 
   getPopularExercises(): Observable<PopularExerciseDTO[]> {
-    return this.http.get<PopularExerciseDTO[]>(`${this.url}/popular-exercises`);
+    return this.http.get<PopularExerciseDTO[]>(`${this.url}/popular-exercises`, { params: { amount: this.amount } });
   }
 
   getIncomingGroupSessions(): Observable<IncomingGroupSessionDTO[]> {
-    return this.http.get<IncomingGroupSessionDTO[]>(`${this.url}/incoming-group-sessions`);
+    return this.http.get<IncomingGroupSessionDTO[]>(`${this.url}/incoming-group-sessions`, { params: { amount: this.amount } });
   }
 
   getRecentTemplates(): Observable<RecentTemplateDTO[]> {
